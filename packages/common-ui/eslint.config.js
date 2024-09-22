f6097c7f7a4cca7d33ae76301598c241845965b0
blacksand <yuan.com@gmail.com>
2024-09-23 01:54:02 +0800

    chore(common-ui): eslint config 配置不应该包含 next 选项

--- packages/common-ui/eslint.config.js
+++ packages/common-ui/eslint.config.js
@@ -1,7 +1,6 @@
 import { eslintConfig, getProjectRoot } from '@peaks/config-eslint'
 
 const config = eslintConfig({
-  next: true,
   projectRoot: getProjectRoot(import.meta.url),
   react: true,
 }).append({
