2145651c02fc653901adca8304331fa0da2cb978
blacksand <yuan.com@gmail.com>
2024-09-27 15:56:04 +0800

    feat(common-utils): enhance is-object predicate function
    Improved the isObject function to handle arrays correctly. Now it checks for null values and arrays more precisely.

--- packages/common-utils/src/lib/predicates/is-object.ts
+++ packages/common-utils/src/lib/predicates/is-object.ts
@@ -1,6 +1,5 @@
 export function isObject<T>(
   value: T | undefined | null,
 ): value is T & object & Record<string, unknown> {
-  // 如果 value 是一个对象，那么返回 true
-  return typeof value === 'object' && value !== null
+  return value != null && !Array.isArray(value) && typeof value === 'object'
 }
