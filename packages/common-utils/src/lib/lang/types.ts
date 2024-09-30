a887311182e6f2ed61a4ef8d3d6455a4f0a20107
blacksand <yuan.com@gmail.com>
2024-09-30 15:24:52 +0800

    refactor(common-utils): rename 'Maybe' type to 'Perhaps'
    - Replaced 'Maybe' with 'Perhaps' in types definition

--- packages/common-utils/src/lib/lang/types.ts
+++ packages/common-utils/src/lib/lang/types.ts
@@ -1,7 +1,7 @@
 export type Nullable<T> = T | null
 export type Undefinable<T> = T | undefined
 
-export type Maybe<T> = T | null | undefined
+export type Perhaps<T> = T | null | undefined
 
 export type IsStartsWith<
   T extends string,
